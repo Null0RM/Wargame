5bb20b51ee8fd2efd357f568f6e45c4f2b827a70
Hyoyoung Jung <nullorm@Hyoyoungui-MacBookPro.local>
2024-07-18 13:27:41 +0900

    transfer

---
+++ web3oj/web3oj/script/transferERC20Token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../src/transferERC20Token.sol";
+
+contract CounterScript is Script {
+    ERC20 target;
+    transferERC20Token cont;
+
+    function setUp() public { 
+        cont = transferERC20Token(0x19bbc0e256D5F58FFa4005E5c7CE3238a95DA9AE);
+        target = ERC20(0x3fE705B07828e156D0f7EDfeC23ED24582dbb54e);
+    }
+
+    function run() public {
+        vm.startBroadcast();
+
+        bool success = target.transfer(address(cont), 20 * 10**18);
+        require(success);
+        cont.sendToken(0xDf1867A4d659D8D3D98F1d00980C2c6Ff977CD5b, 20 * 10 ** 18);
+
+        vm.stopBroadcast();
+    }
+}
--- web3oj/web3oj/src/makeERC20Token.sol
+++ web3oj/web3oj/src/makeERC20Token.sol
@@ -14,5 +14,7 @@ contract myERC20Token is ERC20, Ownable {
 deploy: forge create src/makeERC20Token.sol:myERC20Token --rpc-url $SEPOLIA_RPC_URL --private-key $SEPOLIA_PRIVATE_KEY --constructor-args "Web3 Online Judge Token" "WEB3OJT" 18
 CA: 0x3fE705B07828e156D0f7EDfeC23ED24582dbb54e
 instance: 0xadcA248D0F5bd09F6391999DE08C6C894fB11bCd
-solve: cast send 0xadcA248D0F5bd09F6391999DE08C6C894fB11bCd --rpc-url $SEPOLIA_RPC_URL --private-key $SEPOLIA_PRIVATE_KEY "setWeb3ojt(address)" "0x3fE705B07828e156D0f7EDfeC23ED24582dbb54e"
+solve: 
+> forge script ./script/Counter.s.sol --rpc-url $SEPOLIA_RPC_URL --private-key $SEPOLIA_PRIVATE_KEY --broadcast
+> cast send 0xDf1867A4d659D8D3D98F1d00980C2c6Ff977CD5b "setWeb3ojt(address)" 0x19bbc0e256D5F58FFa4005E5c7CE3238a95DA9AE --rpc-url $SEPOLIA_RPC_URL --private-key $SEPOLIA_PRIVATE_KEY
  */
